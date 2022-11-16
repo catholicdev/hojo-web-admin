88e113bd528c70a04650cfb6f6505cf51589f960
Dung Nguyen Hung <dung.nh1705@gmail.com>
2022-11-16 17:58:26 +0700

    update css

--- apps/hojo-web/views/sign-in-view/sign-in-view.tsx
+++ apps/hojo-web/views/sign-in-view/sign-in-view.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as Dove } from "@web/public/images/dove.svg";
 import * as React from "react";
 import { useSetRecoilState } from "recoil";
 
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 
 import { loginGuest, reloginGuest } from "@web/services/auth";
 
@@ -37,42 +37,52 @@ export const SignInView: React.FC = () => {
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" className="sign-in">
-      <Box className="sign-in-head" height={{ xs: "40%", md: "50%" }}>
+      <Box padding={6}>
         <Box className="logo">
           <Dove />
         </Box>
       </Box>
-      <Box className="sign-in-body" height={{ xs: "60%", md: "50%" }}>
-        <Box className="sign-in-content" top={{ xs: "230px", lg: "320px" }}>
-          <Box className="sign-in-title" mt={{ xs: "70px", lg: "80px" }}>
-            HOJO
-          </Box>
-          <div className="sign-in-description">
-            Hành Trình Nên Thánh.
-            <br />
-            Cuốn Kinh Thánh 4.0
-            <br />
-            dành cho các bạn trẻ.
-          </div>
-          <Box mt="140px">
-            <Button
-              variant="contained"
-              sx={{
-                borderRadius: "40px",
-                width: "100%",
-                background: "linear-gradient(90deg, #0762C8 0%, #00C2E4 100%)",
-                fontSize: "16px",
-                fontWeight: 500,
-                lineHeight: "20px",
-                height: "56px",
-                padding: "20px 50px",
-              }}
-              size="large"
-              onClick={handleLoginGuest}>
-              Tiếp tục
-            </Button>
-          </Box>
-        </Box>
+
+      <Box className="sign-in-content">
+        <Grid container spacing={7} justifyContent="space-around" alignItems="center" flexDirection="column">
+          <Grid item xs={12} mt={8}>
+            <Typography fontWeight="800" fontSize="3rem" fontFamily="inherit" color="var(--primary-color)">
+              HOJO
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              fontWeight="400"
+              fontSize="1.2rem"
+              fontFamily="inherit"
+              textAlign="center"
+              color="var(--primary-color)">
+              Hành Trình Nên Thánh 4.0
+              <br />
+              Dành cho người trẻ Việt.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} mt={8}>
+            <Box>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "40px",
+                  width: "100%",
+                  background: "linear-gradient(90deg, #0762C8 0%, #00C2E4 100%)",
+                  fontSize: "16px",
+                  fontWeight: 500,
+                  lineHeight: "20px",
+                  height: "56px",
+                  padding: "20px 50px",
+                }}
+                size="large"
+                onClick={handleLoginGuest}>
+                Tiếp tục
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
